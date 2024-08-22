74ee4e4a19043d0c5fff47479e35b4d3ebde357c
Hytrece <123867148+Hytrece@users.noreply.github.com>
2024-08-22 12:59:46 +0000

    asd fd

--- components/categorybadge.tsx
+++
@@ -1,9 +0,0 @@
-const Categorybadge = (word:{word:string}) => {
-  return(<button className="p-[3px] relative">
-    <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-    <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-      <h1>{word}</h1>
-    </div>
-  </button>)
-}
-export default Categorybadge;
