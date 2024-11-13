6bcbd14509f74d21ad2f03d37e174e7be17c34ee
Hytrece <123867148+Hytrece@users.noreply.github.com>
2024-11-13 03:32:37 +0000

    fix morejobs button

--- components/morejobs.tsx
+++ components/morejobs.tsx
@@ -2,7 +2,7 @@
 import {useRouter} from "next/navigation"
 const MoreJobs = ({country}:{country:string}) =>{
     const router = useRouter();
-    return(<button className="p-[2px] mt-5 relative" onClick={(()=>{ router.push(`/${country}/jobs`);})}>
+    return(<button className="p-[2px] mt-5 relative" onClick={(()=>{ router.push(`?${new URLSearchParams({country:country})}`);})}>
         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-full" />
         <div className="px-8 py-1  bg-black rounded-full relative group transition duration-200 text-white text-sm hover:bg-transparent">
           More Jobs
